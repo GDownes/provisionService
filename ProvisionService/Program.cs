3aefc2cbd605fdcb960f9b490ac7c679e419c609
gdownes <gdownes@gmail.com>
2015-03-02 14:14:55 +0000

    Adding service base barebones - Self hosted NancyFX with Razor

---
+++ ProvisionService/Program.cs
@@ -0,0 +1,42 @@
+﻿#region
+
+using System;
+using System.Linq;
+using System.Threading;
+
+using Nancy.Hosting.Self;
+
+#endregion
+
+namespace ProvisionService
+{
+    public static class Program
+    {
+        #region Public Methods and Operators
+
+        public static void Main(string[] args)
+        {
+            var uri = new Uri("http://localhost:7070");
+
+            using (var host = new NancyHost(uri))
+            {
+                host.Start();
+
+                Console.WriteLine("Your application is running on " + uri);
+
+                if (args.Any(s => s.Equals("-d", StringComparison.CurrentCultureIgnoreCase)))
+                {
+                    Thread.Sleep(Timeout.Infinite);
+                }
+                else
+                {
+                    Console.ReadKey();
+                }
+
+                host.Stop();
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ ProvisionService/ProvisionModule.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics;
+
+using Nancy;
+
+namespace ProvisionService
+{
+    public class ProvisionModule : NancyModule
+    {
+        public ProvisionModule()
+        {
+            this.Get["/"] = _ => this.View["Index.cshtml"];
+
+            this.Get["/kueue/dev"] = _ =>
+            {
+                new Process { StartInfo = new ProcessStartInfo("ls") }.Start();
+                return "Started development kueue deployment";
+            };
+
+            this.Get["/kueue/qa"] = _ =>
+            {
+                new Process { StartInfo = new ProcessStartInfo("ls") }.Start();
+                return "Started quality kueue deployment";
+            };
+        }
+    }
+}
