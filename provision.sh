a06943630bf25538da845d504ec2aa6dcaace4e3
gdownes <gdownes@gmail.com>
2015-03-03 14:07:08 +0000

    Moving from mono-runtime to mono-complete due to requiring xbuild

--- provision.sh
+++ provision.sh
@@ -2,7 +2,7 @@ sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 3FA7E0328081BFF6A1
 echo "deb http://download.mono-project.com/repo/debian wheezy main" | sudo tee /etc/apt/sources.list.d/mono-xamarin.list
 echo "deb http://download.mono-project.com/repo/debian wheezy-libtiff-compat main" | sudo tee -a /etc/apt/sources.list.d/mono-xamarin.list
 sudo apt-get update
-sudo apt-get install mono-runtime -y
+sudo apt-get install mono-complete -y
 cd /vagrant
 xbuild /p:Configuration=Release ProvisionService.sln
 cd ~
