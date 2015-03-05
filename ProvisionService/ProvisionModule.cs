d41b774f7d2115035e7ab11b1c37a8002a077428
gdownes <gdownes@gmail.com>
2015-03-05 16:24:36 +0000

    Removing kueue stream test

--- ProvisionService/ProvisionModule.cs
+++ ProvisionService/ProvisionModule.cs
@@ -1,6 +1,4 @@
 ﻿using System.Diagnostics;
-using System.Text;
-using System.Threading;
 
 using Nancy;
 
@@ -16,25 +14,6 @@ namespace ProvisionService
             {
                 new Process { StartInfo = new ProcessStartInfo("ansible-playbook", "/vagrant/deploy-kueue-playbook.yml") }.Start();
                 return "Started development kueue deployment";
-            };
-
-            this.Get["/stream"] = _ =>
-            {
-                var response = new Response();
-                response.Headers.Add("Transfer-Encoding", "Chunked");
-                response.ContentType = "text/plain";
-                response.Contents = s =>
-                {
-                    byte[] bytes = Encoding.UTF8.GetBytes("Hello World\n");
-                    for (int i = 0; i < 10; ++i)
-                    {
-                        s.Write(bytes, 0, bytes.Length);
-                        s.Flush();
-                        Thread.Sleep(2000);
-                    }
-                };
-
-                return response;
             };
         }
     }
